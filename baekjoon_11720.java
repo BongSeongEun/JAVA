f079a3dc3ade223a8d4131a71ae40717a19aeb2c
SeongEun <bongfam2@naver.com>
2023-07-11 00:08:24 +0900

    baekjoon_11720

--- baekjoon_11720.java
+++ baekjoon_11720.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+public class baekjoon_11720{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        //입력받은 값을 String형 변수에 저장한 수 char형 변수로 변환
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for (int i = 0; i<cNum.length; i++){
+            sum += cNum[i] - '0';
+        }
+        System.out.print(sum);
+    }
+}
